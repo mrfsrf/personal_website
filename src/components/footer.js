4b67a0b6aa03b5810ceaa26b8e8f0b749e78f699
Luka Juras <mrfsrf@gmail.com>
2019-07-27 13:20:44 +0200

    separeting footer from Layout.js

---
+++ src/components/footer.js
@@ -0,0 +1,49 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Footer = () => (
+    <footer
+      style={{
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
+      }}
+    >
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >Send mail</Link></h1>
+          <div
+            style={{
+                //to do
+            }}
+            >
+         © {new Date().getFullYear()} Luka Juras,
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </div>
+      </div>
+    </footer>
+  )
+
+// Props!
+//   Header.propTypes = {
+//     siteTitle: PropTypes.string,
+//   }
+
+//   Header.defaultProps = {
+//     siteTitle: ``,
+//   }
+
+  export default Header
